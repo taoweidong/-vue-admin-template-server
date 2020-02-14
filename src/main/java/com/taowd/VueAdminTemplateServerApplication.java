9143f8e5eb00429c82eb59a55e2f89f78ca5d88b
Taoweidong <taowd@outlook.com>
2020-02-14 22:20:26 +0800

    代码初始化

---
+++ src/main/java/com/taowd/VueAdminTemplateServerApplication.java
@@ -0,0 +1,13 @@
+package com.taowd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueAdminTemplateServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VueAdminTemplateServerApplication.class, args);
+  }
+
+}
